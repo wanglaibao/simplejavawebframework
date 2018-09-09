6dab84f3914b3455a781b34c969b97601ca41528
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-09 10:35:06 +0800

    add new java files info

---
+++ simplemvc/src/main/java/com/laibao/simplemvc/controller/CustomerCreateServlet.java
@@ -0,0 +1,29 @@
+package com.laibao.simplemvc.controller;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author laibao wang
+ * @date 2018-09-09
+ * @version 1.0
+ */
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO
+    }
+}
