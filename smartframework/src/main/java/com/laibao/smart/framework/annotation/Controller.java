a4280579d2ca8b7fad0730dd6cacc381cc68f189
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-11 10:53:04 +0800

    update java files info

---
+++ smartframework/src/main/java/com/laibao/smart/framework/annotation/Controller.java
@@ -0,0 +1,18 @@
+package com.laibao.smart.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @ClassName Controller
+ * @Description 控制器注解
+ * @author laibao wang
+ * @date 2018-09-11
+ * @version 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+}
