434e953e61c4b3613b0ab722b83c56c89ff25688
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-11 11:31:18 +0800

    update java files info

--- smartframework/src/main/java/com/laibao/smart/framework/helper/ClassHelper.java
+++ smartframework/src/main/java/com/laibao/smart/framework/helper/ClassHelper.java
@@ -1,8 +1,11 @@
 package com.laibao.smart.framework.helper;
 
+import com.laibao.smart.framework.annotation.Controller;
+import com.laibao.smart.framework.annotation.Service;
 import com.laibao.smart.framework.util.ClassUtil;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 类操作辅助类
@@ -11,8 +14,54 @@ import java.util.Set;
  * @version 1.0
  */
 public interface ClassHelper {
-   Set<Class<?>> CLASS_SET = ClassUtil.getClassSet(ConfigHelper.getAppBasePackage());
 
+    /**
+     * 定义类集合 (用于存放所加载的类)
+     */
+    Set<Class<?>> CLASS_SET = ClassUtil.getClassSet(ConfigHelper.getAppBasePackage());
 
+    /**
+     * 获取应用包名下的所有类
+     * @return Set<Class<?>>
+     */
+    static Set<Class<?>> getClassSet(){
+        return CLASS_SET;
+    }
 
+    /**
+     * 获取应用包名下所有的@Service注解类
+     * @return Set<Class<?>>
+     */
+    static Set<Class<?>> getServiceClassSet(){
+//        Set<Class<?>> classSet=new HashSet<Class<?>>();
+//        for(Class<?> cls:CLASS_SET) {
+//            if(cls.isAnnotationPresent(Service.class)) {
+//                classSet.add(cls);
+//            }
+//        }
+//        return classSet;
+        return CLASS_SET.stream()
+                          .filter(clazz -> clazz.isAnnotationPresent(Service.class))
+                          .collect(Collectors.toSet());
+    }
+
+    /**
+     * 获取应用包名下所有的@Controller注解类
+     * @return Set<Class<?>>
+     */
+     static Set<Class<?>> getControllerClassSet() {
+         return CLASS_SET.stream()
+                          .filter(clazz -> clazz.isAnnotationPresent(Controller.class))
+                          .collect(Collectors.toSet());
+     }
+
+    /**
+     * 获取应用包名下所有的bean类(@Service,@Controller等)
+     * @return Set<Class<?>>
+     */
+     static Set<Class<?>> getBeanClassSet(){
+         return CLASS_SET.stream()
+                 .filter(clazz -> clazz.isAnnotationPresent(Service.class) || clazz.isAnnotationPresent(Controller.class))
+                 .collect(Collectors.toSet());
+     }
 }
