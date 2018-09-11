3d528f6cf47ff7be810e486c2a2822a11f5a5475
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-11 10:53:30 +0800

    update java files info

---
+++ smartframework/src/main/java/com/laibao/smart/framework/helper/ClassHelper.java
@@ -0,0 +1,7 @@
+package com.laibao.smart.framework.helper;
+
+/**
+ * Created by A on 2018/9/11.
+ */
+public class ClassHelper {
+}
---
+++ smartframework/src/main/java/com/laibao/smart/framework/util/ClassUtil.java
@@ -0,0 +1,41 @@
+package com.laibao.smart.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+/**
+ * @ClassName ClassUtil
+ * @Description 类操作工具类
+ * @author laibao wang
+ * @date 2018-09-11
+ * @version 1.0
+ */
+public interface ClassUtil {
+    Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
+
+    /**
+     * @Description: 获取类加载器
+     * @return ClassLoader
+     */
+    static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    static Class<?> loadClass(String className,boolean isInitialized) {
+        Class<?> clazz = null;
+        try {
+            clazz =Class.forName(className,isInitialized,getClassLoader());
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("load class failure",e);
+            throw new RuntimeException(e);
+        }
+        return clazz;
+    }
+
+    static Set<Class<?>> getClassSet(String packageName){
+        //TODO
+        return null;
+    }
+}
