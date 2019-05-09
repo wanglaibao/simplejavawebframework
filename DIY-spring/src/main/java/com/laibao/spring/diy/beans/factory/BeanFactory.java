fdcf8cf2b54f476a5975bf2fe99bb55daabd2a79
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-10 07:21:38 +0800

    add new readme.md file info

--- DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/BeanFactory.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/BeanFactory.java
@@ -4,9 +4,19 @@ import com.laibao.spring.diy.beans.BeanDefinition;
 
 public interface BeanFactory {
 
+    /**
+     *
+     * @param beanId
+     * @return BeanDefinition
+     */
     BeanDefinition getBeanDefinition(String beanId);
 
 
+    /**
+     *
+     * @param beanId
+     * @return Object
+     */
     Object getBean(String beanId);
 
 }
