4048db60b6fa38e374d516075a5ff10d1ad30ac5
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-10 16:40:55 +0800

    add new readme.md file info

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -0,0 +1,63 @@
+package com.laibao.spring.diy.beans.factory.xml;
+
+import com.laibao.spring.diy.beans.BeanDefinition;
+import com.laibao.spring.diy.beans.factory.BeanDefinitionStoreException;
+import com.laibao.spring.diy.beans.factory.support.BeanDefinitionRegister;
+import com.laibao.spring.diy.beans.factory.support.GenericBeanDefinition;
+import com.laibao.spring.diy.util.ClassUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+public class XmlBeanDefinitionReader {
+
+    private static final String ID_ATTRIBUTE = "id";
+
+    private static final String CLASS_ATTRIBUTE = "class";
+
+    private BeanDefinitionRegister register;
+
+    public XmlBeanDefinitionReader(BeanDefinitionRegister register) {
+        this.register = register;
+    }
+
+    private void loadBeanDefinition(String configFile) {
+
+        InputStream inputStream = null;
+        try {
+            ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
+            inputStream = classLoader.getResourceAsStream(configFile);
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(inputStream);
+
+            Element rootElement = document.getRootElement(); // <beans>
+            Iterator<Element> iterator = rootElement.elementIterator();
+            while (iterator.hasNext()) {
+                Element element = iterator.next();
+                String id = element.attributeValue(ID_ATTRIBUTE);
+                String beanClassName = element.attributeValue(CLASS_ATTRIBUTE);
+                BeanDefinition beanDefinition = new GenericBeanDefinition(id,beanClassName);
+                //beanDefinitionMap.put(id,beanDefinition);
+                this.register.registerBeanDefinition(id,beanDefinition);
+            }
+
+        } catch (DocumentException e) {
+            //e.printStackTrace();
+            throw new BeanDefinitionStoreException("IOException parsing XML document ",e.getCause());
+        }finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
