80da7ea1de59acdcaa22b1c424e2ff511a0c242a
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-10 17:53:29 +0800

    add new readme.md file info

--- DIY-spring/src/test/java/com/laibao/spring/diy/test/v1/BeanFactoryV2Test.java
+++ DIY-spring/src/test/java/com/laibao/spring/diy/test/v1/BeanFactoryV2Test.java
@@ -2,12 +2,11 @@ package com.laibao.spring.diy.test.v1;
 
 import com.laibao.spring.diy.beans.factory.BeanCreationException;
 import com.laibao.spring.diy.beans.factory.BeanDefinitionStoreException;
-import com.laibao.spring.diy.beans.factory.BeanFactoryV1;
-import com.laibao.spring.diy.beans.factory.support.DefaultBeanFactoryV1;
 import com.laibao.spring.diy.beans.factory.support.DefaultBeanFactoryV2;
 import com.laibao.spring.diy.beans.factory.xml.XmlBeanDefinitionReader;
 import com.laibao.spring.diy.service.v1.PetStoreService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertNotNull;
@@ -15,13 +14,19 @@ import static org.junit.Assert.assertEquals;
 
 public class BeanFactoryV2Test {
 
+    private DefaultBeanFactoryV2 factory = null;
+
+    private XmlBeanDefinitionReader reader = null;
+
+    @Before
+    public void setUp() {
+         factory = new DefaultBeanFactoryV2();
+
+         reader = new XmlBeanDefinitionReader(factory);
+    }
 
     @Test
     public void testGetBean() {
-
-        DefaultBeanFactoryV2 factory = new DefaultBeanFactoryV2();
-
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
 
         reader.loadBeanDefinition("petstore-v1.xml");
 
@@ -36,8 +41,6 @@ public class BeanFactoryV2Test {
     @Test
     public void testInvalidXML() {
         try{
-            DefaultBeanFactoryV2 factory =  new DefaultBeanFactoryV2();
-            XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
             reader.loadBeanDefinition("xxx-v1.xml");
 
         }catch (BeanDefinitionStoreException e) {
@@ -49,8 +52,6 @@ public class BeanFactoryV2Test {
     @Test
     public void testInvalidBean(){
         try{
-            DefaultBeanFactoryV2 factory =  new DefaultBeanFactoryV2();
-            XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
             reader.loadBeanDefinition("petstore-v1.xml");
             factory.getBean("invalidBean");
         }catch (BeanCreationException e) {
