7c9f2449b833603dd1bfa80e887883da65fea84d
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-11 16:52:19 +0800

    add new readme.md file info

--- DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/BeanFactoryV1.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/BeanFactoryV1.java
@@ -2,6 +2,13 @@ package com.laibao.spring.diy.beans.factory;
 
 import com.laibao.spring.diy.beans.BeanDefinition;
 
+/**
+ *  BeanFactoryV1
+ *  违背了单一职责原则 SRP Single Re Pricipal
+ *  所以需要对进行重构使之满足单一职责原则
+ *
+ */
+
 public interface BeanFactoryV1 {
 
     /**
