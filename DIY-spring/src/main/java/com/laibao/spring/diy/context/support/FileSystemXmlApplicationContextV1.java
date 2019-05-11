68ec5e36bc526e200741c2175193925a6803fcab
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-12 00:32:28 +0800

    add new readme.md file info

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/context/support/FileSystemXmlApplicationContextV1.java
@@ -0,0 +1,21 @@
+package com.laibao.spring.diy.context.support;
+
+import com.laibao.spring.diy.beans.factory.support.DefaultBeanFactoryV2;
+import com.laibao.spring.diy.beans.factory.xml.XmlBeanDefinitionReader;
+import com.laibao.spring.diy.context.ApplicationContextV1;
+
+public class FileSystemXmlApplicationContextV1 implements ApplicationContextV1 {
+
+    private DefaultBeanFactoryV2 factory;
+
+    public FileSystemXmlApplicationContextV1(String configFile) {
+        factory = new DefaultBeanFactoryV2();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinition(configFile);
+    }
+
+    @Override
+    public Object getBean(String beanId) {
+        return factory.getBean(beanId);
+    }
+}
