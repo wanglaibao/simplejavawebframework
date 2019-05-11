5eb95f05caea9bd48de9e3549031099e8f3be352
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-12 01:16:51 +0800

    add new readme.md file info

--- DIY-spring/src/test/java/com/laibao/spring/diy/test/v1/V1AllTest.java
+++ DIY-spring/src/test/java/com/laibao/spring/diy/test/v1/V1AllTest.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({BeanFactoryV1Test.class,
                     BeanFactoryV2Test.class,
-                    ApplicationContextV1Test.class})
+                    ApplicationContextV1Test.class,
+                    ResourceV1Test.class})
 public class V1AllTest {
 }
