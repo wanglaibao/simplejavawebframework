6ab9780ac0f0e61f8748c43227310dc9f997b0fa
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-12 14:47:46 +0800

    add new readme.md file info

--- DIY-spring/src/main/java/com/laibao/spring/diy/context/support/AbstractApplicationContextV1.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/context/support/AbstractApplicationContextV1.java
@@ -5,6 +5,12 @@ import com.laibao.spring.diy.beans.factory.xml.XmlBeanDefinitionReaderV2;
 import com.laibao.spring.diy.context.ApplicationContextV1;
 import com.laibao.spring.diy.core.io.Resource;
 
+/**
+ * AbstractApplicationContextV1抽象类,通过模板模式,抽象出通用的访问逻辑
+ * 并且提供一个抽象方法getResourceByPath供各个子类进行实现
+ * getResourceByPath方法返回的资源Resource接口抽象了资源的多样性
+ * 可以通过统一的方式进行访问
+ */
 public abstract class AbstractApplicationContextV1 implements ApplicationContextV1 {
 
     private DefaultBeanFactoryV2 factory;
--- DIY-spring/src/main/java/com/laibao/spring/diy/context/support/ClassPathXmlApplicationContextV3.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/context/support/ClassPathXmlApplicationContextV3.java
@@ -3,6 +3,12 @@ package com.laibao.spring.diy.context.support;
 import com.laibao.spring.diy.core.io.ClassPathResource;
 import com.laibao.spring.diy.core.io.Resource;
 
+/**
+ * 继承AbstractApplicationContextV1类实现 ==> 实现了ApplicationContextV1接口
+ * 通过XmlBeanDefinitionReaderV2来读取和加载XML配置文件,
+ * 并且通过Resource接口统一了资源的访问路径,屏蔽了资源路径的多样性
+ *
+ */
 public class ClassPathXmlApplicationContextV3 extends AbstractApplicationContextV1 {
 
 
