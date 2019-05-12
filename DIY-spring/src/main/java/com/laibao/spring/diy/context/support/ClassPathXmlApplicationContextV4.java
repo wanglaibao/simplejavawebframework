e1002b29ca88e53f0c5cedbe8fe7ba01a25c9926
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-12 15:00:13 +0800

    add new readme.md file info

--- DIY-spring/src/main/java/com/laibao/spring/diy/context/support/ClassPathXmlApplicationContextV4.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/context/support/ClassPathXmlApplicationContextV4.java
@@ -3,6 +3,11 @@ package com.laibao.spring.diy.context.support;
 import com.laibao.spring.diy.core.io.ClassPathResource;
 import com.laibao.spring.diy.core.io.Resource;
 
+/**
+ * 继承AbstractApplicationContextV2抽象类 ===> 实现了ApplicationContextV2接口,从而获取了设置ClassLoader的功能
+ * getResourceByPath方法返回的资源Resource接口抽象了资源的多样性
+ * 可以通过统一的方式进行访问
+ */
 public class ClassPathXmlApplicationContextV4 extends AbstractApplicationContextV2{
 
     public ClassPathXmlApplicationContextV4(String configFile) {
