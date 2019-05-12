a9775d5e6e3a264bc07a38184d721b097e75f145
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-12 22:59:35 +0800

    add new readme.md file info

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/PropertyValue.java
@@ -0,0 +1,42 @@
+package com.laibao.spring.diy.beans;
+
+public class PropertyValue {
+
+    private final String name;
+
+    private final Object value;
+
+    private boolean converted = false;
+
+    private Object convertedValue;
+
+    public PropertyValue(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public boolean isConverted() {
+        return converted;
+    }
+
+    public void setConverted(boolean converted) {
+        this.converted = converted;
+    }
+
+    public Object getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(Object convertedValue) {
+        this.converted = true;
+        this.convertedValue = convertedValue;
+    }
+}
