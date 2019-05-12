b4ebe8b474a9a338a7a462ef928d0868efe454f9
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-12 08:27:16 +0800

    add new readme.md file info

--- DIY-spring/src/main/java/com/laibao/spring/diy/context/support/FileSystemXmlApplicationContextV3.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/context/support/FileSystemXmlApplicationContextV3.java
@@ -3,7 +3,7 @@ package com.laibao.spring.diy.context.support;
 import com.laibao.spring.diy.core.io.FileSystemResource;
 import com.laibao.spring.diy.core.io.Resource;
 
-public class FileSystemXmlApplicationContextV3 extends AbstractApplicationContext{
+public class FileSystemXmlApplicationContextV3 extends AbstractApplicationContextV1 {
 
 
     public FileSystemXmlApplicationContextV3(String configFile) {
