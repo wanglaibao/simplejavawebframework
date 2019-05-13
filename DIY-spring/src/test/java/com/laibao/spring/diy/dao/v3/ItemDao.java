fc86e419753dc2d567bb8f283150fa8b080aa512
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-13 21:13:54 +0800

    add new readme.md file info

---
+++ DIY-spring/src/test/java/com/laibao/spring/diy/dao/v3/ItemDao.java
@@ -0,0 +1,4 @@
+package com.laibao.spring.diy.dao.v3;
+
+public class ItemDao {
+}
---
+++ DIY-spring/src/test/java/com/laibao/spring/diy/service/v3/PetStoreService.java
@@ -0,0 +1,33 @@
+package com.laibao.spring.diy.service.v3;
+
+import com.laibao.spring.diy.dao.v3.AccountDao;
+import com.laibao.spring.diy.dao.v3.ItemDao;
+
+/**
+ * 专门用来进行constructor注入测试使用的
+ */
+public class PetStoreService {
+    private AccountDao accountDao;
+
+    private ItemDao itemDao;
+
+    private int version;
+
+    public PetStoreService(AccountDao accountDao, ItemDao itemDao, int version) {
+        this.accountDao = accountDao;
+        this.itemDao = itemDao;
+        this.version = version;
+    }
+
+    public AccountDao getAccountDao() {
+        return accountDao;
+    }
+
+    public ItemDao getItemDao() {
+        return itemDao;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+}
