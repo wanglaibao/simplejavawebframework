d9b00e7d75c3728cbb15eabd03744f20d2a13143
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-13 13:13:03 +0800

    add new readme.md file info

--- DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/BeanDefinitionStoreException.java
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/BeanDefinitionStoreException.java
@@ -3,6 +3,11 @@ package com.laibao.spring.diy.beans.factory;
 import com.laibao.spring.diy.beans.BeanException;
 
 public class BeanDefinitionStoreException extends BeanException {
+
+    public BeanDefinitionStoreException(String message) {
+        super(message);
+    }
+
     public BeanDefinitionStoreException(String message, Throwable cause) {
         super(message, cause);
     }
