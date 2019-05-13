d5c4d33bc1ba8411fa910cb52ff12509299eed22
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-14 01:55:13 +0800

    add new readme.md file info

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/support/GenericBeanDefinitionV4.java
@@ -0,0 +1,71 @@
+package com.laibao.spring.diy.beans.factory.support;
+
+import com.laibao.spring.diy.beans.BeanDefinitionV4;
+import com.laibao.spring.diy.beans.ConstructorArgument;
+import com.laibao.spring.diy.beans.PropertyValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 新增 ConstructorArgument getConstructorArgument()方法
+ *
+ * 主要用于构造函数方式注入的时候
+ *
+ * 获对象的构造函数参数列表所包含的参数值
+ */
+public class GenericBeanDefinitionV4 implements BeanDefinitionV4 {
+
+    private String id;
+
+    private String beanClassName;
+
+    private boolean singleton = true;
+
+    private boolean prototype = false;
+
+    private String scope = SCOPE_DEFAULT;
+
+    private List<PropertyValue> propertyValueList = new ArrayList<>();
+
+    private ConstructorArgument constructorArgument = new ConstructorArgument();
+
+    public GenericBeanDefinitionV4(String id, String beanClassName) {
+        this.id = id;
+        this.beanClassName = beanClassName;
+    }
+
+    @Override
+    public String getBeanClassName() {
+        return this.beanClassName;
+    }
+
+    public boolean isSingleton() {
+        return this.singleton;
+    }
+
+    public boolean isPrototype() {
+        return this.prototype;
+    }
+
+    public String getScope() {
+        return this.scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+        this.singleton = SCOPE_SINGLETON.equals(scope) || SCOPE_DEFAULT.equals(scope);
+        this.prototype = SCOPE_PROTOTYPE.equals(scope);
+
+    }
+
+    @Override
+    public List<PropertyValue> getPropertyValues() {
+        return propertyValueList;
+    }
+
+    @Override
+    public ConstructorArgument getConstructorArgument() {
+        return this.constructorArgument;
+    }
+}
