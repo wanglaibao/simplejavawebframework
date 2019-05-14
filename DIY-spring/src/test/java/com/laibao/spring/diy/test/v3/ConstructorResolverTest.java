f08d5afe7bed67282c1f533ed5a9a080450c774e
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-14 10:48:02 +0800

    add new readme.md file info

---
+++ DIY-spring/src/test/java/com/laibao/spring/diy/test/v3/ConstructorResolverTest.java
@@ -0,0 +1,34 @@
+package com.laibao.spring.diy.test.v3;
+
+import com.laibao.spring.diy.beans.BeanDefinitionV4;
+import com.laibao.spring.diy.beans.factory.support.ConstructorResolver;
+import com.laibao.spring.diy.beans.factory.support.DefaultBeanFactoryV7;
+import com.laibao.spring.diy.beans.factory.xml.XmlBeanDefinitionReaderV5;
+import com.laibao.spring.diy.core.io.ClassPathResource;
+import com.laibao.spring.diy.service.v3.PetStoreService;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+public class ConstructorResolverTest {
+
+
+    @Test
+    public void testConstructorResolverArguments() {
+        DefaultBeanFactoryV7 factory = new DefaultBeanFactoryV7();
+        XmlBeanDefinitionReaderV5 reader = new XmlBeanDefinitionReaderV5(factory);
+        reader.loadBeanDefinition(new ClassPathResource("petstore-v3.xml"));
+
+
+        BeanDefinitionV4 beanDefinition = factory.getBeanDefinitionV4("petStore");
+
+        ConstructorResolver resolver = new ConstructorResolver(factory);
+        PetStoreService petStoreService = (PetStoreService) resolver.autowireConstructor(beanDefinition);
+
+        assertEquals(1, petStoreService.getVersion());
+        assertNotNull(petStoreService.getAccountDao());
+        assertNotNull(petStoreService.getItemDao());
+
+    }
+}
