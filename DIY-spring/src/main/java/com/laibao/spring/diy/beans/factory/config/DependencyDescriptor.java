c22a6dfac464c658a990e67e9ef0a61e3819cc0a
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-17 17:48:33 +0800

    Default Changelist

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/beans/factory/config/DependencyDescriptor.java
@@ -0,0 +1,27 @@
+package com.laibao.spring.diy.beans.factory.config;
+
+import java.lang.reflect.Field;
+
+/**
+ *  对@Autowired 注解的描述
+ */
+public class DependencyDescriptor {
+    private Field field;
+
+    private boolean required;
+
+    public DependencyDescriptor(Field field, boolean required) {
+        this.field = field;
+        this.required = required;
+    }
+    public Class<?> getDepencyType() {
+        if (this.field != null) {
+            return field.getType();
+        }
+        throw new RuntimeException("only support field depency");
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+}
