6008f0affb34a1c7561ec8b6216b6fe2c32ebb63
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-17 12:20:47 +0800

    Default Changelist

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/core/type/classreading/MetadataReader.java
@@ -0,0 +1,30 @@
+package com.laibao.spring.diy.core.type.classreading;
+
+import com.laibao.spring.diy.core.io.Resource;
+import com.laibao.spring.diy.core.type.AnnotationMetadata;
+import com.laibao.spring.diy.core.type.ClassMetadata;
+
+/**
+ * Simple facade for accessing class metadata,
+ * as read by an ASM {@link org.springframework.asm.ClassReader}.
+ *
+ * @author Juergen Hoeller
+ * @since 2.5
+ */
+public interface MetadataReader {
+    /**
+     * Return the resource reference for the class file.
+     */
+    Resource getResource();
+
+    /**
+     * Read basic class metadata for the underlying class.
+     */
+    ClassMetadata getClassMetadata();
+
+    /**
+     * Read full annotation metadata for the underlying class,
+     * including metadata for annotated methods.
+     */
+    AnnotationMetadata getAnnotationMetadata();
+}
