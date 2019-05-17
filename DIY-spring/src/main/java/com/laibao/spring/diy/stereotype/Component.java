938f800731f5ef62aa7e4fedc0d6353d35e377c0
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-17 08:42:43 +0800

    Default Changelist

---
+++ DIY-spring/src/main/java/com/laibao/spring/diy/stereotype/Component.java
@@ -0,0 +1,10 @@
+package com.laibao.spring.diy.stereotype;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Component {
+    String value() default "";
+}
